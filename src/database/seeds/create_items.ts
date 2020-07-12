629d9ba55c177d652b7b7a5917f4a1b5f07a860b
Renan horste <renan.horste@gmail.com>
2020-07-12 00:13:06 -0300

    keep going

--- src/database/seeds/create_items.ts
+++ src/database/seeds/create_items.ts
@@ -1,7 +1,12 @@
-import { Seeder } from "knex";
-
 import Knex from 'knex'
 
 export async function seed(knex: Knex){
-    
+    await knex('items').insert([
+        {title: 'lampadas', image: 'lampadas.svg'},
+        {title: 'pilhas e baterias', image: 'baterias.svg'},
+        {title: 'papeis e papelao', image: 'papeis-papelao.svg'},
+        {title: 'Resíduos eletronicos', image: 'eletronicos.svg'},
+        {title: 'Respiduos organicos', image: 'residuos.svg'},
+        {title: 'Oleo de cozinha', image: 'Oleo.svg'}
+    ]);
 }
