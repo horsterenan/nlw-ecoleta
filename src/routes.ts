a7b3f8e6c065e34893a0b5d34974fedcefdf53fe
Renan Horste <renan.horste@gmail.com>
2020-07-15 01:40:39 -0300

    inserção associativa pontos-items

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,59 @@
 import express from 'express'
+import knex from './database/connection'
 
 const routes = express.Router();
 
 
-routes.get("/", (req, res) => {
-    return res.json({
-        message:"hello world"
+routes.get("/items",async(req, res) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+      return {
+        id: item.id,
+        title: item.title,
+        image_url:`http://localhost:3333/uploads/${item.image}`
+      }
     })
+
+    return res.json(serializedItems)
+
   })
+
+routes.post("/points",async(req,res) =>{
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = req.body;
+
+  const insertedIds = await knex('points').insert({
+    image: "image-fake",
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+  })
+  
+  const point_id = insertedIds[0]
+  
+  const pointItems = items.map((item_id: number)=>{
+    return{
+      item_id,
+      point_id,
+    }
+  })
+  await knex('point_items').insert(pointItems)
+
+  return res.json({success:true})
+ 
+
+})
   export default routes
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express'
 import routes from './routes'
+import path from 'path'
 const app = express() //CRIAÇÃO DA APLICAÇÃO
 
 app.use(express.json());
 
 app.use(routes)
 
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
 //porta que a aplicação vai estar "escutando"
 app.listen(3333)
+
